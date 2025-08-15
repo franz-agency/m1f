16adf225be1453e261cb1b97f597d249750f3d40
Autofranz <auto@franz.agency>
2025-08-15 23:51:23 +0200

    chore(release): prepare version 3.8.3
    - Bump version from 3.8.2 to 3.8.3
    - Update CHANGELOG with bin script fixes for global execution

--- tools/_version.py
+++ tools/_version.py
@@ -8,5 +8,5 @@ This file is the only place where the version number should be updated.
 All other files should import from here.
 """
 
-__version__ = "3.8.2"
+__version__ = "3.8.3"
 __version_info__ = tuple(int(x) for x in __version__.split(".")[:3])
